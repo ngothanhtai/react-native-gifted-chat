6dc6c3f4293809b501ba258ead9199084f263446
Ngo Thanh Tai <ngothanhtai89@yahoo.com>
2017-03-23 15:30:16 +0700

    Message image shows based on width, height

--- src/MessageImage.js
+++ src/MessageImage.js
@@ -8,8 +8,28 @@ import LightBox from 'react-native-lightbox';
 import PhotoView from 'react-native-photo-view';
 
 export default class MessageImage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imgWidth: 150, imgHeight: 100,
+    }
+  }
+
+  componentDidMount() {
+    const { image } = this.props.currentMessage;
+    Image.getSize(image, (imgWidth, imgHeight) => {
+      const scaleRatio = 200/imgWidth;
+      this.setState({
+        imgWidth: imgWidth * scaleRatio,
+        imgHeight: imgHeight * scaleRatio,
+      });
+    });
+
+  }
   render() {
-    const {image} = this.props.currentMessage;
+    const { imgWidth, imgHeight } = this.state;
+    const { image } = this.props.currentMessage;
     return (
       <View style={[styles.container, this.props.containerStyle]}>
         <LightBox
@@ -23,7 +43,7 @@ export default class MessageImage extends React.Component {
               }}
         >
         <Image
-          style={[styles.image, this.props.imageStyle,]}
+          style={[styles.image, this.props.imageStyle, { width: imgWidth, height: imgHeight, }]}
           source={{uri: image}}
         />
         </LightBox>
